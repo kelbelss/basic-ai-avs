548e03c18b64adaa76e42115a83b09bcd9911991
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-05-09 17:15:05 +0400

    fix: solc version

--- src/ServiceManager.sol
+++ src/ServiceManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.30;
+pragma solidity ^0.8.29;
 
 import {ISignatureUtils} from "eigenlayer-contracts/interfaces/ISignatureUtils.sol";
 import {IAVSDirectory} from "eigenlayer-contracts/interfaces/IAVSDirectory.sol";
