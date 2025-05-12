3918aa60fc21dc4dd4099d1ab01b140549c0bb86
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-05-12 16:55:56 +0400

    refactor: improve integration base set up

--- test/integration/IntegrationBase.t.sol
+++ test/integration/IntegrationBase.t.sol
@@ -12,42 +12,53 @@ import {
 import {IERC20} from "../../lib/eigenlayer-contracts/lib/openzeppelin-contracts-v4.9.0/contracts/token/ERC20/IERC20.sol";
 
 contract IntegrationBase is Test {
-    string HOLESKY_RPC = vm.envString("HOLESKY_RPC_URL");
-    address AVS_DIRECTORY = vm.envAddress("AVS_DIRECTORY");
-    address DELEGATION_MANAGER = vm.envAddress("DELEGATION_MANAGER");
-    address STRATEGY_MANAGER = vm.envAddress("STRATEGY_MANAGER");
-    IERC20 STETH_TOKEN = IERC20(vm.envAddress("STETH_TOKEN"));
-    IStrategy STETH_STRAT = IStrategy(vm.envAddress("STETH_STRAT"));
-    address STETH_WHALE = vm.envAddress("STETH_WHALE");
+    string HOLESKY_RPC;
+    address AVS_DIRECTORY;
+    address DELEGATION_MANAGER;
+    address STRATEGY_MANAGER;
+    IERC20 STETH_TOKEN;
+    IStrategy STETH_STRAT;
+    address STETH_WHALE;
 
     uint256 FAUCET_AMOUNT = 1e17; // 0.1 stETH
 
     ServiceManager serviceManager;
 
-    address operator = vm.addr(1);
+    address operator;
 
     function setUp() public virtual {
-        // 1) fork Holesky
-        vm.createSelectFork(HOLESKY_RPC, /*block*/ block.number);
+        // 1) load environment & keys
+        HOLESKY_RPC = vm.envString("HOLESKY_RPC_URL");
+        AVS_DIRECTORY = vm.envAddress("AVS_DIRECTORY");
+        DELEGATION_MANAGER = vm.envAddress("DELEGATION_MANAGER");
+        STRATEGY_MANAGER = vm.envAddress("STRATEGY_MANAGER");
+        STETH_TOKEN = IERC20(vm.envAddress("STETH_TOKEN"));
+        STETH_STRAT = IStrategy(vm.envAddress("STETH_STRAT"));
+        STETH_WHALE = vm.envAddress("STETH_WHALE");
+        operator = vm.envAddress("SIMPLE_OPERATOR");
 
         // 2) fund the operator with some eth
         vm.deal(operator, 1e18); // 1 eth
+        vm.makePersistent(operator);
 
-        // 2) get some stETH for your operator
+        // 3) fork Holesky
+        vm.createSelectFork(HOLESKY_RPC, block.number);
+
+        // 4) get some stETH for your operator
         vm.startPrank(STETH_WHALE);
         STETH_TOKEN.transfer(operator, FAUCET_AMOUNT);
         vm.stopPrank();
 
-        // 3) operator deposits into the strategy so they can register
+        // 5) operator deposits into the strategy so they can register
         vm.startPrank(operator);
         STETH_TOKEN.approve(STRATEGY_MANAGER, FAUCET_AMOUNT);
         IStrategyManager(STRATEGY_MANAGER).depositIntoStrategy(STETH_STRAT, STETH_TOKEN, FAUCET_AMOUNT);
 
-        // 4) operator registers with EigenLayer
+        // 6) operator registers with EigenLayer
         IDelegationManager(DELEGATION_MANAGER).registerAsOperator(operator, 1, "");
         vm.stopPrank();
 
-        // 5) deploy AVS ServiceManager pointing at the real AVSDirectory
+        // 7) deploy AVS ServiceManager pointing at the real AVSDirectory
         serviceManager = new ServiceManager(AVS_DIRECTORY);
     }
 }
