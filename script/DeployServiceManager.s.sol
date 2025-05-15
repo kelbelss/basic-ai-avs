9ff707cd401a48680d649c402afd33ab97b090d6
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-05-15 15:53:57 +0400

    chore: comment out deploy script until updated changes

--- script/DeployServiceManager.s.sol
+++ script/DeployServiceManager.s.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.29;
+// pragma solidity ^0.8.29;
 
-import "forge-std/Script.sol";
-import "forge-std/console.sol";
-import {ServiceManager} from "../src/ServiceManager.sol";
+// import "forge-std/Script.sol";
+// import "forge-std/console.sol";
+// import {ServiceManager} from "../src/ServiceManager.sol";
 
-contract DeployServiceManager is Script {
-    function run() external {
-        
-        uint256 deployerKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        address avsDirectory = vm.envAddress("AVS_DIRECTORY");
+// contract DeployServiceManager is Script {
+//     function run() external {
 
-        vm.startBroadcast(deployerKey);
-        ServiceManager serviceManager = new ServiceManager(avsDirectory);
-        console.log("ServiceManager deployed at:", address(serviceManager));
-        vm.stopBroadcast();
-    }
-}
+//         uint256 deployerKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+//         address avsDirectory = vm.envAddress("AVS_DIRECTORY");
+
+//         vm.startBroadcast(deployerKey);
+//         ServiceManager serviceManager = new ServiceManager(avsDirectory);
+//         console.log("ServiceManager deployed at:", address(serviceManager));
+//         vm.stopBroadcast();
+//     }
+// }
